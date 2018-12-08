8ab3fc1f5c00b8631602aef3e253916c986ad0f0
Erislandio Soares <erislandiosoares21@gmail.com>
2018-12-08 17:14:20 -0200

    Primeiro commit

---
+++ src/assets/icons/MenuIcon.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MenuIcon extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MenuIcon;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div id="header">
+                teste
+            </div>
+        );
+    }
+}
+
+export default Header;
