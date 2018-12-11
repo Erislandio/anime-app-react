0716d44f06d82b5f84e1f9ad163ee9b1ca1e8d33
Erislandio Soares <erislandiosoares21@gmail.com>
2018-12-10 23:03:22 -0200

    Ajustes header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Header from './components/header/Header'
+import Loader from './components/Loader';
+import Main from './components/main/Main';
 
 class App extends Component {
 
@@ -9,21 +11,42 @@ class App extends Component {
     super(props);
 
     this.state = {
-      anime: []
+      anime: null,
+      loading: true
     }
-    
+
+    this.getAnimeData = this.getAnimeData.bind(this)
   }
 
 
-  
+  getAnimeData() {
+    fetch('https://kitsu.io/api/edge//anime?filter[text]=naruto').then(res => {
+      return res.json()
+    }).then(data => {
+      console.log(data)
+      this.setState({ anime: data, loading: false })
+    })
+  }
+
+  componentDidMount() {
+    this.getAnimeData();
+  }
+
 
   render() {
+    const { anime } = this.state
+    console.log(this)
+    if (this.state.loading) {
+      return <Loader />
+    }
     return (
       <div className="App">
         <Header />
+        <Main data={this.state.anime.data}/>
       </div>
     );
   }
+
 }
 
 export default App;
